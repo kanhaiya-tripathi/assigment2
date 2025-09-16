21857e9a251d83d33f4dca851a9874bd46273a17
kanhaiya-tripathi <kanhaiyatripathi152@gmail.com>
2025-09-16 22:32:15 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Header from "../components/Header";
 import CategoryHeader from "@/components/CategoryHeader";
 import CategoryList from "@/components/CategoryList";
 import ProductCard from "@/components/ProductCard";
-import axios from "axios";
 
 const HomePage = async () => {
   // const response = await axios.get("https://fakestoreapi.com/products ");
